7283c62d5d13cfd80e7612da6a58e1266e30b998
Kajetan Spionek <kajetanspionek@gmail.com>
2019-01-26 22:00:30 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(new MaterialApp(
+    home: new RootPage(),
+    debugShowCheckedModeBanner: true,
+    theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+
+  ));
+}
+
